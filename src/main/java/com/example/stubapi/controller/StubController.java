f346f46defac9c35b5e23f55cd59dcb4301929a1
dima <baranovdi666@gmail.com>
2024-07-23 12:21:35 +0300

    Initial commit

--- src/main/java/com/example/stubapi/controller/StubController.java
+++ src/main/java/com/example/stubapi/controller/StubController.java
@@ -1,36 +1,44 @@
 package com.example.stubapi.controller;
 
-
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Random;
 
 @RestController
-
 @RequestMapping("/api")
 public class StubController {
 
-
     @GetMapping("/getData")
     public String getData() {
-        return "{\"message\":\"This is a static JSON response\"}";
+        return "{\"message\":\"СТАТИЧНЫЙ ОТВЕТ\"}";
     }
-
 
     @PostMapping("/postData")
     public Map<String, Object> postData(@RequestBody Map<String, String> request) {
         Map<String, Object> response = new HashMap<>();
         response.put("login", request.get("login"));
         response.put("password", request.get("password"));
-        response.put("date", new Date());
+
+        // Получение текущей даты и времени
+        LocalDateTime now = LocalDateTime.now();
+        // Форматирование даты и времени
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = now.format(formatter);
+        response.put("date", formattedDate);
+
         try {
-            Thread.sleep(1000 + (int)(Math.random() * 1000));
+            Random random = new Random();
+            Thread.sleep(1000 + random.nextInt(1000));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         return response;
     }
 }
+
+
